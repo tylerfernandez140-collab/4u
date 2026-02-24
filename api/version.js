a994d8eb11977fdd2f449672c3d68f2ffd4d8bbc
Ivan <tylerfernandez140@gmail.com>
2026-02-24 20:58:07 +0800

    Fix refresh loop by disabling force update
     Fixed Refresh Loop:
    - Disabled forceUpdate flag in /api/version
    - App was stuck checking version and refreshing repeatedly
    - Now forceUpdate: false prevents infinite reload
    
     App Should Now Work Normally:
    - Cross-device hug functionality is loaded
    - No more constant refreshing
    - App stays stable and functional
    
     Cross-device communication should work between web and mobile now!

--- api/version.js
+++ api/version.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   res.json({ 
     version: '2.0.0',
-    forceUpdate: true,
-    message: 'Please refresh to get cross-device hug functionality'
+    forceUpdate: false, // Disabled to prevent refresh loop
+    message: 'App is up to date with cross-device hug functionality'
   });
 };
